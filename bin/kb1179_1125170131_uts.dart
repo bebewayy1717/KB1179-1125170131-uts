43ff0aec0dc7b7d76b6617067731a6121ef4437c
Wahyu Slamet Adi Triyono <wahyuslamet.adi@gmail.com>
2025-10-05 11:02:57 +0700

    delete lib/kb1179-1125170131-uts

--- bin/kb1179_1125170131_uts.dart
+++
@@ -1,5 +0,0 @@
-import 'package:kb1179_1125170131_uts/kb1179_1125170131_uts.dart' as kb1179_1125170131_uts;
-
-void main(List<String> arguments) {
-  print('Hello world: ${kb1179_1125170131_uts.calculate()}!');
-}

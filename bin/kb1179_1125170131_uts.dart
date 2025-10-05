8ba1fc3679f74c9178c78292e03e9c8828ff8cd9
Wahyu Slamet Adi Triyono <wahyuslamet.adi@gmail.com>
2025-10-05 10:59:29 +0700

    first commit

---
+++ bin/kb1179_1125170131_uts.dart
@@ -0,0 +1,5 @@
+import 'package:kb1179_1125170131_uts/kb1179_1125170131_uts.dart' as kb1179_1125170131_uts;
+
+void main(List<String> arguments) {
+  print('Hello world: ${kb1179_1125170131_uts.calculate()}!');
+}
---
+++ test/kb1179_1125170131_uts_test.dart
@@ -0,0 +1,8 @@
+import 'package:kb1179_1125170131_uts/kb1179_1125170131_uts.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
